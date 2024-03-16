b0a4f1315d1f130815572abbe5797448aee41f5f
Vivek Kumar <kumar97vivek@gmail.com>
2024-03-16 18:51:26 +0530

    Installed passport for auth

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Facade\FlareClient\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function registerUser(Request $request): JsonResponse
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+        ]);
+
+        $token = $user->createToken(config('app.name'))->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function loginUser(Request $request): JsonResponse
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken(config('app.name'))->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
+
+    public function getUserDetail(Request $request): JsonResponse
+    {
+        $user = Auth::user();
+        return response()->json(['data' => $user], 200);
+    }
+
+    public function logoutUser(Request $request): JsonResponse
+    {
+        $request->user()->token()->revoke();
+        return response()->json(['message' => 'Successfully logged out'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', [UserController::class, 'registerUser']);
+Route::post('/login', [UserController::class, 'loginUser']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', [UserController::class, 'getUserDetail']);
+    Route::get('/logout', [UserController::class, 'logoutUser']);
 });
